99014876cbf89c096a26b9bee1e08c2cf006bfdf
tancheng <tancheng1990@gmail.com>
2020-02-13 10:35:50 -0800

    [feature] extract info from DFG JSON to construct accelerator

--- acc/test/Acc_test.py
+++ acc/test/Acc_test.py
@@ -10,15 +10,15 @@ Author : Cheng Tan
 """
 
 from pymtl3 import *
-from pymtl3.stdlib.test              import TestSinkCL
-from pymtl3.stdlib.test.test_srcs    import TestSrcRTL
+from pymtl3.stdlib.test           import TestSinkCL
+from pymtl3.stdlib.test.test_srcs import TestSrcRTL
 
-from ...lib.opt_type                 import *
-from ...lib.messages                 import *
-from ..Acc                           import Acc
-from ...fu.single.Alu          import Alu
-from ...fu.single.Mul          import Mul
-from ...fu.single.MemUnit      import MemUnit
+from ...lib.opt_type              import *
+from ...lib.messages              import *
+from ..Acc                        import Acc
+from ...fu.single.Alu             import Alu
+from ...fu.single.Mul             import Mul
+from ...fu.single.MemUnit         import MemUnit
 
 import json
 
@@ -28,14 +28,12 @@ import json
 
 class TestHarness( Component ):
 
-  def construct( s, DUT, FuDFG, DataType, CtrlType, src_data, src_opt, sink_out ):
+  def construct( s, DUT, FuDFG, DataType, CtrlType, src_data, sink_out ):
 
     s.src_data  = [ TestSrcRTL( DataType, src_data[i]  )
                   for i in range( 4  ) ]
     s.sink_out  = [ TestSinkCL( DataType, sink_out[i] )
                   for i in range( 1 ) ]
-#    s.src_opt   = [ TestSrcRTL( CtrlType, src_opt[i]  )
-#                  for i in range( 3  ) ]
 
     s.dut = DUT( FuDFG, DataType, CtrlType )
 
@@ -43,8 +41,6 @@ class TestHarness( Component ):
       connect( s.src_data[i].send, s.dut.recv_data[i] )
     for i in range( 1 ):
       connect( s.dut.send_data[i], s.sink_out[i].recv )
-#    for i in range( 3 ):
-#      connect( s.src_opt[i].send, s.dut.recv_opt[i] )
 
   def done( s ):
     done_flag = True
@@ -80,21 +76,25 @@ def run_sim( test_harness, max_cycles=10 ):
   test_harness.tick()
 
 class Element:
-  def __init__( s, _id, FuType, opt, const, input, output ):
-    s.id      = _id
+
+  def __init__( s, id, FuType, opt, const, input, output ):
+    s.id      = id
     s.fu_type = FuType
     s.opt     = opt
     s.const   = const
     s.input   = input
     s.output  = output
+    s.num_input = len(const)
 
 class DFG:
 
   def __init__( s ):
-    s.elements = []
+    s.elements  = []
+    s.num_input = 0
 
   def add_element( s, element ):
     s.elements.append( element )
+    s.num_input += element.num_input
 
   def get_element( s, _id ):
     for e in s.elements:
@@ -106,6 +106,7 @@ def test_acc():
   fu_dfg = DFG()
   unit_map = { "ALU": Alu, "MUL": Mul }
   opt_map  = { "OPT_ADD": OPT_ADD, "OPT_SUB": OPT_SUB, "OPT_MUL": OPT_MUL }
+
   with open('dfg.json') as json_file:
     dfg = json.load(json_file)
     print(dfg)
@@ -114,24 +115,11 @@ def test_acc():
                    dfg[i]['in_const'], dfg[i]['in'], dfg[i]['out'] )
       fu_dfg.add_element( e )
 
-#  e0 = Element( 0, Alu)
-#  e1 = Element( 1, Alu)
-#  e2 = Element( 2, Mul)
-#  fu_dfg.add_element( e0 )
-#  fu_dfg.add_element( e1 )
-#  fu_dfg.add_element( e2 )
-#  e0.append(e2)
-#  e1.append(e2)
-  
   DUT      = Acc
   DataType = mk_data( 16, 1 )
   CtrlType = mk_ctrl()
-  src_opt  = [[CtrlType(OPT_ADD)], [CtrlType(OPT_SUB)], [CtrlType(OPT_MUL)]]
-  src_data = [ [DataType(2, 1)],
-               [DataType(3, 1)],
-               [DataType(4, 1)],
-               [DataType(2, 1)] ]
-  sink_out = [ [DataType(10, 1)] ]
-  th = TestHarness( DUT, fu_dfg, DataType, CtrlType, src_data, src_opt, sink_out )
+  src_data = [ [DataType(2, 1)] ] * fu_dfg.num_input 
+  sink_out = [ [DataType(0, 1)] ]
+  th = TestHarness( DUT, fu_dfg, DataType, CtrlType, src_data, sink_out )
   run_sim( th )
 
